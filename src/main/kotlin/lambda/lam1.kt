17ff787a68fa271e38eee4acdae0139358b0429a
arozaliansyah@gmail.com <2002Jul!>
2022-09-12 16:44:00 +0700

    Kotlin

---
+++ src/main/kotlin/lambda/lam1.kt
@@ -0,0 +1,13 @@
+package lambda
+
+fun main(args: Array<String>) {
+    displayMessage("Rekayasa Perangkat Lunak", 10);
+}
+
+fun displayMessage(msg: String, count: Int) {
+    var counter = 1
+    while (counter <= count) {
+        println("$msg $counter")
+        counter++
+    }
+}
